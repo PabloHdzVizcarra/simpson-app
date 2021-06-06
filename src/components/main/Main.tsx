f0c073c56d6a77d33ecca5f17a201b2b1dc6e597
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-06-06 13:21:29 -0500

    feat now character display

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
-import { Character } from "../../types/Character";
+import CharacterComponent from "./character/CharacterComponent";
 import { useCaseGetCharacter } from "../../use_cases/GetCharacter";
+import { Character } from "../../types/Character";
 
 function Main() {
   const [character, setCharacter] = React.useState<Character>();
@@ -13,7 +14,16 @@ function Main() {
   }, [])
 
   return (
-      <main className="container d-flex">
+      <main className="container d-flex justify-content-center">
+        {character
+            ? <CharacterComponent
+                key={character.character.length}
+                image={character.image}
+                char={character.character}
+                quote={character.quote}
+            />
+            : null
+        }
       </main>
   );
 }
