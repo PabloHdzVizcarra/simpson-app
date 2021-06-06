224ed132f7ebfabb201986fadcdca4a33bec3213
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-06-06 12:58:07 -0500

    feat GetCharacter.ts use case to get character from the api

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Character } from "../../types/Character";
+import { useCaseGetCharacter } from "../../use_cases/GetCharacter";
 
 function Main() {
   const [character, setCharacter] = React.useState<Character>();
-  console.log(character);
-  console.log(setCharacter);
+
+  useEffect(() => {
+    useCaseGetCharacter
+    .create()
+        .then(setCharacter)
+        .catch(console.log)
+  }, [])
 
   return (
       <main className="container d-flex">
-
       </main>
   );
 }
