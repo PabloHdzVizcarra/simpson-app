d101fafcddce561db54c8f936808867d12140d43
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-06-06 12:15:51 -0500

    refactor delete unnecessary code

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,18 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Character } from "../../types/Character";
-import { getSimpsonCharacter } from "../../use_cases/getSimpsonCharacter";
-import { CallAPI } from "../../service/call_api/CallAPI";
-import SimpsonAPI from "../../service/simpsonAPI";
 
 function Main() {
-  const [data, setData] = useState<Character>();
-  const getCharacter = new getSimpsonCharacter(new CallAPI(new SimpsonAPI()));
-
-  useEffect(() => {
-    getCharacter.character()
-        .then(data => setData(data))
-  }, [getCharacter]);
-
+  const [character, setCharacter] = React.useState<Character>();
+  console.log(character);
+  console.log(setCharacter);
 
   return (
       <main className="container d-flex">
--- src/web_api/simpson/simpsonAPI.ts
+++ src/web_api/simpson/simpsonAPI.ts
@@ -1,11 +1,15 @@
-import { Character } from "../types/Character";
+import { Character } from "../../types/Character";
 
 class SimpsonAPI {
   protected readonly url: string = "https://thesimpsonsquoteapi.glitch.me/quotes";
 
-  public async get(): Promise<Character[] | void> {
+  public async get(): Promise<Character> {
     try {
-      const response = await fetch(this.url);
+      const response = await fetch(this.url, {
+        headers: {
+          "Access-Control-Allow-Origin": "*"
+        }
+      });
       return await response.json();
     } catch (e) {
       throw new Error(e.toString());
