8ddd49d3f19ad32fa142d851147f44b87454af14
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-06-06 09:43:41 -0500

    refactor to use case

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,27 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { CallAPI } from "../../service/call_api/CallAPI";
-import SimpsonAPI from "../../service/simpsonAPI";
 import { Simpson } from "../../types/Simpson";
 
 function Main() {
-  const [data, setData] = useState<Simpson[]>([]);
-  const serviceAPI = new CallAPI(new SimpsonAPI());
+  const [data, setData] = useState<Simpson>();
 
   useEffect(() => {
-    serviceAPI
-    .get()
-    .then(data => {
-      if (!data) return
-      setData(data)
-    })
-    .catch(console.log)
-  }, [serviceAPI])
+
+  }, [])
 
 
   return (
       <main className="container d-flex">
-        <div>Image</div>
-        <div>Text</div>
+
       </main>
   );
 }
