e4e54a29e41b2bfcf06b9b86e74281c8d4b5085f
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-06-06 09:34:50 -0500

    feat correct call api

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,16 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { CallAPI } from "../../service/call_api/CallAPI";
 import SimpsonAPI from "../../service/simpsonAPI";
+import { Simpson } from "../../types/Simpson";
 
 function Main() {
-  const [data, setData] = useState();
+  const [data, setData] = useState<Simpson[]>([]);
   const serviceAPI = new CallAPI(new SimpsonAPI());
-  serviceAPI
-      .get()
-      .then(data => {
-    console.log(data);
-  })
-      .catch(console.log)
+
+  useEffect(() => {
+    serviceAPI
+    .get()
+    .then(data => {
+      if (!data) return
+      setData(data)
+    })
+    .catch(console.log)
+  }, [serviceAPI])
+
 
   return (
       <main className="container d-flex">
